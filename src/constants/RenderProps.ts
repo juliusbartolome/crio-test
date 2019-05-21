16bbcfa6546964230df028b94787f4c634ea0df6
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2019-05-22 04:05:58 +0800

    added new prop for selected polygon

--- src/constants/RenderProps.ts
+++ src/constants/RenderProps.ts
@@ -5,7 +5,7 @@
 
 const RENDER_PROPS = {
   POLYGON_OUTLINE: { border: { color: "#aaa", width: 1 }, close: true },
-  POLYGON_SELECTED_OUTLINE: { border: { color: "#14a", width: 1 }, close: true }
+  POLYGON_SELECTED_OUTLINE: { border: { color: "#5a6", width: 3 }, close: true }
 };
 
 export default RENDER_PROPS;
